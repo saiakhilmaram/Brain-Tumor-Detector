bb33b523c654b9fa1f45cf7564684920148358b7
Sai Akhil Maram <54792711+saiakhilmaram@users.noreply.github.com>
2023-08-24 15:02:18 +0530

    Update img_classification.py

--- img_classification.py
+++ img_classification.py
@@ -2,44 +2,22 @@ import numpy as np
 import tensorflow
 from PIL import Image, ImageOps
 
-
-
 def classification(img, weights_file):
 
     # Load the model
-
     model = tensorflow.keras.models.load_model(weights_file)
 
-
     # Create the array of the right shape to feed into the keras model
-
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-    image = img
-
-    #image sizing
-
-    size = (224, 224)
-
-    image = ImageOps.fit(image, size, Image.ANTIALIAS)
-
-
-    #turn the image into a numpy array
-
-    image_array = np.asarray(image)
-
-    # Normalize the image
-
-    normalized_image_array = (image_array.astype(np.float32) / 255)
-
+    # Resize and normalize the image
+    image = ImageOps.fit(img, (224, 224), Image.ANTIALIAS)
+    normalized_image_array = np.array(image, dtype=np.float32) / 255
 
     # Load the image into the array
-
     data[0] = normalized_image_array
 
+    # Run the inference
+    pred = np.argmax(model.predict(data)[0])
 
-    # run the inference
-
-    pred = model.predict_classes(data)[0][0]
-
-    return pred # return position of the highest probability
+    return pred # Return position of the highest probability
